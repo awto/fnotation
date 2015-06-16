6584a2c1ab0003815cefede949757b3f2d96d839
Vitaliy Akimov <vitaliy.akimov@gmail.com>
2015-06-16 23:14:03 +0300

    updating version for swi pack

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(fnotation).
 title('Function notation for Prolog').
-version('0.0.1').
-download('https://github.com/awto/fnotation/archive/v0.0.1.zip').
+version('0.0.2').
+download('https://github.com/awto/fnotation/archive/v0.0.2.zip').
 
 author('Vitaliy Akimov','vitaliy.akimov@gmail.com').
 packager('Vitaliy Akimov','vitaliy.akimov@gmail.com').
