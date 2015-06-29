10e1d4f2d7bb1987e5033b7c3cbcbd4fc1bce7c3
Vitaliy Akimov <vitaliy.akimov@gmail.com>
2015-06-29 22:01:53 +0300

    custom control operators

--- README.md
+++ README.md
@@ -4,9 +4,19 @@ Function notation for Prolog
 This is simple term_expansion/2 based macros for functions notation inspired 
 by Mozart/Oz.
 
+Unlike other libraries for the same purposes this librart is much simplier, 
+output code is still easy to debug, it doesn't require special notation for 
+function's definitions, any predicate may be used as a funciton already. 
+The simpliest usage is for predicate with the only output argument in the last 
+position. But any predicate with any position of output arguments and any number 
+of them may be called as a function. In case of more than one arguments only
+one can be specified by placeholder but the others may still be bound with some
+named variable.
+
 Use `$>` and `$<` operators in subterms of head or body to specify function's 
-term and a variable placeholder respectively. If the variable placeholder isn't 
-specified the library will add another (last) argument for the term. 
+term and its output variable placeholder respectively. If the variable 
+placeholder isn't specified the library will add another (last) argument for 
+the term and use it as output variable.
 
 e.g.
 
@@ -23,5 +33,14 @@ and
 will be translated into 
 
     sample1(A,R) :- sample3(T,A) sample2(T, R) .
+
+The operators name may be changed, for example making both of them to be `$$`:
+
+    :- fnotation_ops($$,$$) .
+    :- op(900,fx,$$) .
+
+    sampled(A,R) :- sample2($$ sample3($$, A), R) .
+
+
 
 
--- prolog/fnotation.pl
+++ prolog/fnotation.pl
@@ -1,6 +1,15 @@
-:- module(fnotation,[op(900,fx,$>)]).
+:- module(fnotation,[op(900,fx,$>),fnotation_ops/2]).
 
 :- use_module(library(apply)).
+
+:- dynamic in_op/1, out_op/1 .
+
+%% fnoation_ops(+Atom,+Atom) is det
+%
+% defines operators for specifying function term and variable position in it
+fnotation_ops(I,O) :-
+        retractall(in_op(_)), retractall(out_op(_)),
+        assertz(in_op(I)), assertz(out_op(O)) .
 
 subterm(T,T).
 subterm(T,S) :-
@@ -15,25 +24,27 @@ expand_notation(Var,B,Term,ResTerm,GsH,GsT) :-
 expand_notation_l(Var,B,[',',Arg1,Arg2],Res,Gs,Gs) :- !,
         expand_notation(Var,B,Arg1,Res1,Res,(Res1,Tmp)),
         expand_notation(Var,B,Arg2,Res2,Tmp,Res2) .
-expand_notation_l(_,_,[$>,Arg],Var,GsH,GsT) :- !,
+expand_notation_l(_,_,[Ftr,Arg],Var,GsH,GsT) :- in_op(Ftr), !,
         expand_notation(Var,B,Arg,Expr,GsH,(Res,GsT)),
         (var(B) *->
          Expr =.. [RFun|RArgs],
          append(RArgs,[Var],NRArgs),
          Res =.. [RFun|NRArgs]
         ; Res = Expr) .
-expand_notation_l(Var,y,[$<],Var,Gs,Gs) :- ! .
+expand_notation_l(Var,y,[Ftr],Var,Gs,Gs) :- out_op(Ftr), ! .
 expand_notation_l(_,_,[H],H,Gs,Gs) :- ! .
 expand_notation_l(Var,B,[H|Args],Res,GsH,GsT) :-
         foldl(expand_notation(Var,B),Args,ArgsRes,GsH,GsT),
         Res =.. [H|ArgsRes] . 
+
+
+:- fnotation_ops($>,$<) .
 
 fn_expand(H :- B, RH :- NB) :-
   expand_notation(_,_,B,RB,NB,Gs),
   expand_notation(_,_,H,RH,Gs,RB),
   (H :- B) \= (RH :- NB)
   .
-
 fn_expand(H,R) :- 
   not(functor(H,:-,2)), fn_expand(H:-true,R) . 
 
