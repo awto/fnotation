f1e678ae77c478f0dce94e72f092f8d2ac69d1b9
Vitaliy Akimov <vitaliy.akimov@gmail.com>
2020-11-06 21:30:06 +0200

    meta_predicates boundaries

--- README.md
+++ README.md
@@ -1,46 +1,40 @@
 Function notation for Prolog
 =============================
 
-This is simple term_expansion/2 based macros for functions notation inspired 
-by Mozart/Oz.
+This is simple term_expansion/2 based macros for functions notation.
 
-Unlike other libraries for the same purposes this librart is much simplier, 
-output code is still easy to debug, it doesn't require special notation for 
-function's definitions, any predicate may be used as a funciton already. 
+This is a simple transform, the output is still easy to debug, 
+it doesn't require special notation for function's definitions, 
+any predicate may be used as a funciton already.
+
 The simpliest usage is for predicate with the only output argument in the last 
 position. But any predicate with any position of output arguments and any number 
 of them may be called as a function. In case of more than one arguments only
 one can be specified by placeholder but the others may still be bound with some
 named variable.
 
-Use `$>` and `$<` operators in subterms of head or body to specify function's 
-term and its output variable placeholder respectively. If the variable 
-placeholder isn't specified the library will add another (last) argument for 
-the term and use it as output variable.
+Use `$>` and `$<` operators in subterms of head or body to specify function's
+term and its output variable placeholder respectively. If the variable
+placeholder isn't specified the library will add another (last) argument for
+the term and use it as an output variable.
 
 e.g.
 
-    sample1(A,$> sample2($> sample3($<, A))) .
+    h1(A, $> h2()) :- p1(A, $> p2($> p3($<, A))) .
 
-and 
+and
     
-    sample1(A,$> sample2($> sample3($<, A), $<)) .
+    h1(A, $> h2($<)) :- p1(A, $> p2($> p3($<, A), $<)) .
 
-and 
+is converted into:
 
-    sample1(A,R) :- sample2($> sample3($<, A), R) .
+    h1(A,V1) :- h2(V1), p3(V0, A), p2(V0, V2), p1(A, V2) .
 
-will be translated into 
-
-    sample1(A,R) :- sample3(T,A) sample2(T, R) .
-
-The operators name may be changed, for example making both of them to be `$$`:
+The operators names may be changed, for example making both of them to be `$$`:
 
     :- fnotation_ops($$,$$) .
     :- op(900,fx,$$) .
 
-    sampled(A,R) :- sample2($$ sample3($$, A), R) .
-
-
+    h1(A,R) :- p1($$ p2($$, A), R) .
 
 
--- prolog/fnotation.pl
+++ prolog/fnotation.pl
@@ -2,55 +2,110 @@
 
 :- use_module(library(apply)).
 
-:- dynamic in_op/1, out_op/1 .
+:- dynamic in_op/1, out_op/1, mpred_memo/2 .
 
 %% fnoation_ops(+Atom,+Atom) is det
 %
 % defines operators for specifying function term and variable position in it
-fnotation_ops(I,O) :-
-        retractall(in_op(_)), retractall(out_op(_)),
-        assertz(in_op(I)), assertz(out_op(O)) .
+fnotation_ops(I, O) :-
+  retractall(in_op(_)), retractall(out_op(_)),
+  assertz(in_op(I)), assertz(out_op(O)) .
 
-subterm(T,T).
-subterm(T,S) :-
-        T =.. [_|Args],
-        member(I,Args),
-        subterm(I,S) .
+expand_notation(_, _, Var, Var, Gs, Gs) :- var(Var), ! .
+expand_notation(Var, y, Term, Var, Gs, Gs) :- out_op(Term), ! .
+expand_notation(Var, HasOut, Term, ResTerm, GsH, GsT) :-
+  compound(Term),!,
+  compound_name_arguments(Term, Ftr, Args),
+  (predicate_property(Term, meta_predicate(_))
+   *->
+     maplist(expand_notation_term, Args, ResArgs),
+     compound_name_arguments(ResTerm, Ftr, ResArgs),
+     GsH = GsT
+  ; in_op(Ftr), [Arg] = Args
+    *-> 
+    expand_notation(ResTerm, SubHasOut, Arg, ResArg, GsH, (SubTerm, GsT)),
+    (var(SubHasOut), compound(ResArg)
+     *->
+       compound_name_arguments(ResArg, RFtr, RArgs),
+       append(RArgs, [ResTerm], NRArgs),
+       compound_name_arguments(SubTerm, RFtr, NRArgs)
+    ; SubTerm = ResArg)
+  ; foldl(expand_notation(Var, HasOut), Args, ArgsRes, GsH, GsT),
+    compound_name_arguments(ResTerm, Ftr, ArgsRes)
+  )
+.
+expand_notation(_, _, Term, Term, Gs, Gs).
 
-expand_notation(_,_,Var,Var,Gs,Gs) :- var(Var), ! .
-expand_notation(Var,B,Term,ResTerm,GsH,GsT) :-
-        Term =.. LTerm,
-        expand_notation_l(Var,B,LTerm,ResTerm,GsH,GsT).
-expand_notation_l(Var,B,[',',Arg1,Arg2],Res,Gs,Gs) :- !,
-        expand_notation(Var,B,Arg1,Res1,Res,(Res1,Tmp)),
-        expand_notation(Var,B,Arg2,Res2,Tmp,Res2) .
-expand_notation_l(_,_,[Ftr,Arg],Var,GsH,GsT) :- in_op(Ftr), !,
-        expand_notation(Var,B,Arg,Expr,GsH,(Res,GsT)),
-        (var(B) *->
-         Expr =.. [RFun|RArgs],
-         append(RArgs,[Var],NRArgs),
-         Res =.. [RFun|NRArgs]
-        ; Res = Expr) .
-expand_notation_l(Var,y,[Ftr],Var,Gs,Gs) :- out_op(Ftr), ! .
-expand_notation_l(_,_,[H],H,Gs,Gs) :- ! .
-expand_notation_l(Var,B,[H|Args],Res,GsH,GsT) :-
-        foldl(expand_notation(Var,B),Args,ArgsRes,GsH,GsT),
-        Res =.. [H|ArgsRes] . 
+expand_notation_term(Term, Res) :-
+  expand_notation(_, _, Term, Tmp, Res, Tmp) .
 
-
-:- fnotation_ops($>,$<) .
+:- fnotation_ops($>, $<) .
 
 fn_expand(H :- B, RH :- NB) :-
-  expand_notation(_,_,B,RB,NB,Gs),
-  expand_notation(_,_,H,RH,Gs,RB),
-  (H :- B) \= (RH :- NB)
-  .
-fn_expand(H,R) :- 
-  not(functor(H,:-,2)), fn_expand(H:-true,R) . 
+  expand_notation(_, _, B, RB, Gs, RB),
+  expand_notation(_, _, H, RH, NB, Gs),
+  (H :- B) \= (RH :- NB), ! .
+fn_expand(H, R) :- 
+  not(functor(H,:-,2)), fn_expand(H :- true,R) . 
 
-:- multifile system:term_expansion/2.
-:- dynamic   system:term_expansion/2.
+:- begin_tests(fn_expand).
 
-system:term_expansion(F,T) :- fn_expand(F,T) .
+test(fn_expand1, [true(Ret = (h1 :- (p3(V1, A), p2(V1, V2), p1(A, V2))))]) :- 
+  fn_expand((h1 :- p1(A, $> p2($> p3($<, A)))), Ret) .
 
+test(fn_expand2, [true(Ret = (h1(A, R) :- p2(V1, A), p1(V1, R)))]) :- 
+  fn_expand((h1(A, R) :- p1($> p2($<, A), R)), Ret).
 
+test(fn_expand_fails, [fail]) :- 
+  fn_expand((h1(A, R) :- p1(p2(A), R)), _).
+
+test(fn_expand_metapreds,
+     [true(Ret = (h1(A, B) :- p1, ((p3(V1, B), p2(V1))->(p5(A, V2), p4(V2));(p7(V3), p6(V3)))))]) :- 
+  fn_expand((h1(A,B) :- p1, (p2($> p3($<,B)) -> p4($>p5(A));p6($>p7()))), Ret).
+
+test(fn_expand_head1, [true(Ret = (h1(V1) :- b(V1), p1))]) :-
+  fn_expand((h1($> b()) :- p1), Ret).
+
+test(fn_expand_head2, [true(Ret = (h1(V1) :- c(A, V2), b(V2, V1), p1(A)))]) :-
+  fn_expand((h1($> b($> c(A,$<))) :- p1(A)), Ret).
+
+test(fn_expand_head_with_body, [true(Ret = (h1(V1) :- h2(V1), p2(V2), p1(V2)))]) :-
+  fn_expand((h1($> h2()) :- p1($> p2())), Ret).
+
+test(fn_expand_head_with_body2,
+     [true(Ret = (h1(V1, V2, C) :- h2(V1, A), h3(B, V2), p2(p3(V3, B)), p4(C, V4), p1(A, V3, V4)))]) :-
+  fn_expand((h1($> h2($<, A), $> h3(B), C) :- p1(A, $> p2(p3($<, B)), $>p4(C))), Ret).
+
+:- op(900, fx, $$).    
+
+test(fn_expand_custom, [
+       setup(fnotation_ops($$,$$)),
+       cleanup(fnotation_ops($>,$<)),
+       true(Ret = (h1(A, R):-p2(V1, A), p1(V1, R)))]) :- 
+  fn_expand((h1(A,R) :- p1($$ p2($$, A), R)), Ret).
+
+:- end_tests(fn_expand).
+
+:- multifile user:term_expansion/2.
+:- dynamic   user:term_expansion/2.
+
+user:term_expansion(F, R) :- fn_expand(F, R) .
+
+:- begin_tests(fnotation).
+
+pred1(A,s(A)) .
+pred2 :- pred1(a, $> pred1(a)) .
+pred3($> pred1(a)).
+
+test(run_pred1) :- pred2 .
+test(run_pred2) :- pred3(s(a)) .
+
+:- op(900, fx, $$).    
+
+test(fn_expand_custom, [
+       setup(fnotation_ops($$,$$)),
+       cleanup(fnotation_ops($>,$<)),
+       true(Ret = (h1(A, R):-p2(V1, A), p1(V1, R)))]) :- 
+  fn_expand((h1(A,R) :- p1($$ p2($$, A), R)), Ret).
+
+:- end_tests(fnotation).
